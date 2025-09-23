96a1c8377807d68071ae9db3ec55272286152f83
Bogily <126955759+Bogily@users.noreply.github.com>
2025-09-23 15:16:14 +0200

    Refactor App component by removing unused useEffect and cleaning up import statements

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 // TypeScript Interfaces für API Daten
 interface ResponseData {
@@ -14,25 +14,7 @@ interface InputData {
 
 function App() {
   // useEffect muss INNERHALB der Komponente stehen
-  useEffect(() => {
-    // Register custom protocol handler
-    if ('registerProtocolHandler' in navigator) {
-      try {
-        navigator.registerProtocolHandler(
-          'carlos',
-          'http://127.0.0.1:3020/carlos?url=%s',
-          // Removed the third argument as it is not supported
-        );
-        console.log('Carlos protocol handler registered!');
-      } catch (error) {
-        console.log('Protocol handler registration failed:', error);
-      }
-    }
-  }, []);
-
-  return (
-    <div>Hello World</div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
